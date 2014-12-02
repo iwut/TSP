6bf7e141d4d22039128de39b8d4189df05996a89
galgazur <galgazur@gmail.com>
2014-12-02 11:09:27 +0100

    3-opt

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,59 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+	Visualizer visual;
+	private boolean VISUALIZE = false;
+	
+	
 	public static void main(String[] args) {
 		new Main();
 	}
 
 	public Main() {
-
-		Kattio io = new Kattio(System.in);
+		
+		long starttime = System.currentTimeMillis();
+		
+		Kattio io;
+		
+		io = new Kattio(System.in);
+		
+		
 		InputReader in = new InputReader();
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 
 		Path path = in.readIndata(br);
 
 		DistanceHolder distanceHolder = new DistanceHolder();
 		distanceHolder.calculateDistances(path);
+		
+		//Node[] nodes = in.readIndata(br);
+		
+		if(System.currentTimeMillis()-starttime>500){
+			VISUALIZE = true;
+		}
+		
+		if(VISUALIZE)
+			visual = Visualizer.createAndShowGUI(path);
+		
+		
+		
+		//Path path = new Path(nodes);
+
 
 		Algorithm algorithm = new GreedyAlgorithm();
 
 		path = algorithm.findPath(path, distanceHolder);
 
+
 		TwoOpt twoOpt = new TwoOpt();
 
 		path = twoOpt.optimizePath(path, distanceHolder);
+
+		if(VISUALIZE)
+			//System.out.print(visual);
+			visual.updatePath(path);
 
 		printPath(path);
 	}
